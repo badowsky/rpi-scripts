6b8cfab4d441fc1cbd1aba7ab298f8f8795d1126
badowsky <mbadowsky@gmail.com>
2014-11-19 18:46:00 +0000

    oke

--- special_button_module/special_button.c
+++ special_button_module/special_button.c
@@ -5,22 +5,9 @@
 MODULE_LICENSE( "GPL" );
 
 
+
+
 static int umh_test( void )
-{
-  struct subprocess_info *sub_info;
-  char *argv[] = { "/usr/bin/logger", "help!", NULL };
-  static char *envp[] = {
-        "HOME=/",
-        "TERM=linux",
-        "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
-
-  sub_info = call_usermodehelper_setup( argv[0], argv, envp, GFP_ATOMIC );
-  if (sub_info == NULL) return -ENOMEM;
-
-  return call_usermodehelper_exec( sub_info, UMH_WAIT_PROC );
-}
-
-static int umh_test2( void )
 {
   char *argv[] = { "/usr/bin/logger", "help!", NULL };
   static char *envp[] = {
