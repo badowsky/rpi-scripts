48c34008020de4c170ec073c95a9187b46225bbb
badowsky <mbadowsky@gmail.com>
2014-11-27 09:30:53 +0000

    yolo

--- therm_module/my_therm.c
+++ therm_module/my_therm.c
@@ -180,9 +180,9 @@ double convertTemp(unsigned char lsb, unsigned char msb){
     //printf("Converting temperature from:\nMSB LSB: %x%x \n", msb, lsb);
     if (inv == 0xf000){
         reading = (reading ^ 0xffff) + 1;
-        //val = - reading / 16.0;
+        val = - reading;
     }else{
-    	//val = reading / 16.0;
+    	val = reading;
     }
 
     return val;
