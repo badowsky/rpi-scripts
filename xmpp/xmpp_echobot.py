86ee41c62089c8adff28820a70dbe517c603401d
Badi <mbadowsky@gmail.com>
2015-01-21 19:44:42 +0100

    fix

--- xmpp/xmpp_echobot.py
+++ xmpp/xmpp_echobot.py
@@ -88,7 +88,8 @@ if __name__ == '__main__':
                     action='store_const', dest='loglevel',
                     const=5, default=logging.INFO)
 	optp.add_option('-b', '--background', help='flag to not block script',
-                    action='store_false', dest='block', default=True)
+                    action='store_const', dest='block', 
+					const=False, default=True)
 
     # JID and password options.
     optp.add_option("-l", "--login", dest="jid",
