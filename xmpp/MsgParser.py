5c3ee699afa69431497bf0a4ebe61c073fbcc985
badowsky <mbadowsky@gmail.com>
2015-01-23 18:24:03 +0000

    yolo

--- xmpp/MsgParser.py
+++ xmpp/MsgParser.py
@@ -7,19 +7,26 @@ logger = logging.getLogger(__name__)
 class MsgParser:
 
     def __init__(self):
-        self.commands_map = {"temp": "/home/pi/rpi-scripts/temp/read_temp.py"}
+        self.commands_map = {"temp": {"cmd":"python3", "args":"/home/pi/rpi-scripts/temp/read_temp.py"},
+                             "all": {"cmd":"python3", "args":"/home/pi/rpi-scripts/read_measurements.py"},
+                             "reboot": {"cmd":"shutdown", "args":"-r now"}}
         logger.info("Parser created.")
 
     def process(self, msg):
         logger.debug("Processing msg: " + msg)
         for key, value in self.commands_map.items():
-            match = re.search(key, msg)
+            match = re.search(key, msg.lower())
             if match:
-                p = subprocess.Popen(['python3', value],
+                p = subprocess.Popen([value['cmd'], value['args']],
                                      stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE)
                 output, error = p.communicate()
                 logger.debug("Message processed, returning: \n" + output.decode())
+                return output.decode()
+
+        
+
+          logger.debug("Message processed, returning: \n" + output.decode())
                 return output.decode()
 
         
